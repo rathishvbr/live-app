cda66c2f4dbc081b82f2d955c7fa1b2389610baf
rathish <rathishvbr@megam.io>
2018-11-17 21:38:28 +0530

    Static react app test

---
+++ precache-manifest.c56444b8254e4f7df7dd14c24b52393a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dd8416bef8a740fc635",
+    "url": "/static/js/main.5dd8416b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "5dd8416bef8a740fc635",
+    "url": "/static/css/main.a7cd5c7d.chunk.css"
+  },
+  {
+    "revision": "29ac7d97458d42787209ef1a677ff479",
+    "url": "/index.html"
+  }
+];
